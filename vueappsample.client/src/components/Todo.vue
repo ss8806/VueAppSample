5d3a8521a04beabbf13d2c92e04c142d5ea81c32
suzuki-s <evo0622@gmail.com>
2024-06-30 18:40:44 +0900

    add todo

--- vueappsample.client/src/components/Todo.vue
+++ vueappsample.client/src/components/Todo.vue
@@ -1,22 +1,35 @@
 <template>
     <h1>Todo List</h1>
+
+    <input class="inputText"
+           placeholder="Wreite Todo"
+           v-on:keydown.shift.enter="addItem"
+           v-model=todoItem />
+
+    <button class="btn add_todo" v-on:click="addItem">
+        Add Todo
+    </button>
+
     <ul v-for="item in items" :key="items.id">
-        <li>{{ item.name }}</li>
+        <li>{{ item.name }} : {{ item.isComplete }}</li>
     </ul>
+
+
 </template>
 
 <script setup lang="ts">
     import { ref, onMounted, defineComponent } from 'vue';
     import axios from "axios";
 
-
     type Items = {
         name: string;
+        isComplete: bool
     };
 
-    let items = ref([]);
+    let items = ref<[Items]>([]);
+    let todoItem = ref("");
 
-    const getTodo = async () => {
+    const getTodo = () => {
         axios.get("https://localhost:7225/api/Todoitems")
             .then(response => {
                 items.value = response.data
@@ -27,6 +40,19 @@
             })
     };
 
+    const addItem = async () => {
+        if (todoItem !== "") {
+            await axios.post("https://localhost:7225/api/Todoitems", {
+                name: todoItem.value,
+                isComplete: false,
+            })
+
+        }
+        // Ä“Ç‚Ýž‚Ý
+        await getTodo();
+
+    };
+
     //DOM“Ç‚Ýž‚ÝŒã‚É“WŠJ‚·‚é
     onMounted(async () => {
         await getTodo();
@@ -35,5 +61,4 @@
 </script>
 
 <style>
-
 </style>
