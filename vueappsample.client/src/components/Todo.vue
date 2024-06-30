9da2c53cb4b132d9fa60bdc6a9582ee475cd145c
suzuki-s <evo0622@gmail.com>
2024-06-30 12:24:05 +0900

    composition api

--- vueappsample.client/src/components/Todo.vue
+++ vueappsample.client/src/components/Todo.vue
@@ -1,74 +1,39 @@
 <template>
     <h1>Todo List</h1>
-    <tr v-for="item in items" :key="items.name">
-        <td>{{ item.name }}</td>
-    </tr>
+    <ul v-for="item in items" :key="items.id">
+        <li>{{ item.name }}</li>
+    </ul>
 </template>
 
-<script lang="ts">
-    import { defineComponent } from 'vue';
+<script setup lang="ts">
+    import { ref, onMounted, defineComponent } from 'vue';
     import axios from "axios";
 
 
-    //axios.defaults.baseURL = 'http://localhost:7225';
-    //axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-    //axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    type Items = {
+        name: string;
+    };
 
-    export default defineComponent({
-        components: {
-        },
-        directives: {
-        },
-        filters: {
-        },
-        props: {
+    let items = ref([]);
 
-        },
-        data() {
-            return {
-                items: [],
-            }
-        },
-        computed: {
-        },
-        watch: {
-        },
-        beforeCreate() {
-        },
-        created() {
-            this.getTodo();
-        },
-        beforeMount() {
-        },
-        mounted() {
-            
-        },
-        updated() {
-        },
-        activated() {
-        },
-        deactivated() {
-        },
-        beforeDestroy() {
-        },
-        destroyed() {
-        },
-        methods: {
+    const getTodo = async () => {
+        axios.get("https://localhost:7225/api/Todoitems")
+            .then(response => {
+                items.value = response.data
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    };
 
-            getTodo: function () {
-                axios.get("https://localhost:7225/api/Todoitems")
-                    .then(response => {
-                        this.items = response.data
-                        console.log(response.data)
-                    })
-                    .catch(err => {
-                        console.error(err)
-                    })
-            }
+    //DOM“Ç‚Ýž‚ÝŒã‚É“WŠJ‚·‚é
+    onMounted(async () => {
+        await getTodo();
+    })
 
-        },
-    });
 </script>
 
 <style>
+
 </style>
